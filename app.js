a70628b65f299479f979e9591af0f820475cab98
MaximeTTS <maxime.s.turquet@gmail.com>
2025-06-06 14:36:02 +0200

    Creation of a helper.js file with the creation of the success function. Understanding json responses.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express"); // 1. Charge la librairie Express.
+let pokemons = require("./mock-pokemon");
+const { success } = require("./helper");
 
 const app = express(); // 2. Crée une instance d’Express (le serveur).
 const port = 3000; // 3. Définit le port 3000 pour écouter les requêtes.
@@ -6,6 +8,19 @@ const port = 3000; // 3. Définit le port 3000 pour écouter les requêtes.
 app.get("/", (req, res) => {
   res.send("Hello, express");
 }); // 4. Si on va sur “/”, le serveur renvoie “Hello, express !”.
+
+app.get("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+  const message = "Un pokemon a bien été trouvé.";
+  res.json(success(message, pokemon));
+}); // 6 route dynamique avec notre reponse fonction success
+
+//nouvelle route de terminaison, affichant le nombre total de pokemon
+app.get("/api/pokemons", (req, res) => {
+  const message = "Voici la liste complete du pokedex :";
+  res.json(success(message, pokemons));
+});
 
 app.listen(port, () => {
   console.log(`Serveur démarré : http://localhost:${port}`);
