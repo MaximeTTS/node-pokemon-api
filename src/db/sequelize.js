e92f0b20a95912376fe297caa9b743b4efaaae7c
MaximeTTS <maxime.s.turquet@gmail.com>
2025-06-30 16:53:37 +0200

    Modification sequelize.js

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,23 +1,36 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const PokemonModel = require("../models/pokemons");
-const pokemons = require("./mock-pokemon");
-const userModel = require("../models/user");
 const bcrypt = require("bcrypt");
+const PokemonModel = require("../models/pokemons");
+const UserModel = require("../models/user");
+const pokemons = require("./mock-pokemon");
 
-const sequelize = new Sequelize("pokedex", "root", "", {
-  host: "localhost",
-  dialect: "mariadb",
-  dialectOptions: {
-    timezone: "Etc/GMT-2",
-  },
-  logging: false,
-});
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize("kk8u5y871hfoaw9y", "t09tvm6qofrtvc7h", "ryujse9ftf40wpqn", {
+    host: "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    dialect: "mariadb",
+    dialectOptions: {
+      timezone: "Etc/GMT-2",
+    },
+    logging: true,
+  });
+} else {
+  sequelize = new Sequelize("pokedex", "username", "password", {
+    host: "192.168.64.2",
+    dialect: "mariadb",
+    dialectOptions: {
+      timezone: "Etc/GMT-2",
+    },
+    logging: true,
+  });
+}
 
 const Pokemon = PokemonModel(sequelize, DataTypes);
-const User = userModel(sequelize, DataTypes);
+const User = UserModel(sequelize, DataTypes);
 
 const initDb = () => {
-  return sequelize.sync({ force: true }).then((_) => {
+  return sequelize.sync().then((_) => {
     pokemons.map((pokemon) => {
       Pokemon.create({
         name: pokemon.name,
@@ -29,8 +42,9 @@ const initDb = () => {
     });
 
     bcrypt
-      .hash("maxime", 10)
-      .then((hash) => User.create({ username: "maxime", password: hash }).then((user) => console.log(user.toJSON())));
+      .hash("pikachu", 10)
+      .then((hash) => User.create({ username: "pikachu", password: hash }))
+      .then((user) => console.log(user.toJSON()));
 
     console.log("La base de donnée a bien été initialisée !");
   });
